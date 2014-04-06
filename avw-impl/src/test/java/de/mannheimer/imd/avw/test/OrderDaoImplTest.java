c3645ae41080b6eb63e7adca97ad354f1a414305
Dennis Ahaus <dennis.ahaus@gmail.com>
2014-04-06 16:31:44 +0200

    Add unit test for OrderDaoImpl

---
+++ avw-impl/src/test/java/de/mannheimer/imd/avw/test/OrderDaoImplTest.java
@@ -0,0 +1,69 @@
+package de.mannheimer.imd.avw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OrderDaoImplTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	public void testFindByIdString() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNewInstance() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPersist() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+
+		fail("Not yet implemented");
+	}
+
+}
